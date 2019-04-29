69b8dd98301e9a17f5b212a7beef02d84dd7d1e4
MathieuJE <mathieu.jean2018@campus-eni.fr>
2019-04-29 16:07:10 +0200

    Ajout de enchereDAO

---
+++ src/dal/EnchereDAO.java
@@ -0,0 +1,30 @@
+package dal;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import bo.Enchere;
+import jdbc.JDBCTools;
+
+
+public class EnchereDAO {
+
+	public static void ajouter(Enchere enchere) throws SQLException, ClassNotFoundException{
+		Connection cnx=null;
+		PreparedStatement rqt=null;
+
+		try {
+			cnx = JDBCTools.getConnection();
+			rqt=cnx.prepareStatement("insert into encheres(no_utilisateur, no_article, date_enchere, montant_enchere) values (?,?,?,?)");
+			rqt.setInt(1, enchere.getNoUtilisateur().getNoUtilisateur());
+			rqt.setInt(2, enchere.getNoArticle().getNoArticle());
+			rqt.setDate(3, enchere.getDateEnchere());
+			rqt.setInt(4, enchere.getMontant_enchere());
+			rqt.executeUpdate();
+		} finally {
+			if (rqt!=null) rqt.close();
+			if (cnx!=null) cnx.close();
+		}
+	}
+}
