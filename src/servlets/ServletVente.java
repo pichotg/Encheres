0ba7eec65bb41b2594bb5769bfb3999820cbf41f
MathieuJE <mathieu.jean2018@campus-eni.fr>
2019-04-30 16:53:18 +0200

    Ajout de la servlet Creation vente

---
+++ src/servlets/ServletVente.java
@@ -0,0 +1,59 @@
+package servlets;
+
+import java.io.IOException;
+import java.sql.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bo.ArticleVendu;
+import dal.ArticleVenduDAO;
+
+/**
+ * Servlet implementation class ServletVente
+ */
+@WebServlet("/ServletVente")
+public class ServletVente extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletVente() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nomArticle = request.getParameter("article");
+		String description = request.getParameter("description");
+		int categorie = Integer.parseInt(request.getParameter("categorie"));
+		int miseAPrix = Integer.parseInt(request.getParameter("miseAPrix"));
+		Date debut = request.getParameter("debut");
+		Date fin = request.getParameter("fin");
+		String rue = request.getParameter("rue");
+		String codePostal = request.getParameter("codePostal");
+		String ville = request.getParameter("ville");
+		int prixVente = Integer.parseInt(request.getParameter("miseAPrix"));
+		String etatVente = "VND";
+		
+		ArticleVendu unArticle = new ArticleVendu(nomArticle,description,);
+		ArticleVenduDAO DAOarticleVente = new ArticleVenduDAO();
+		DAOarticleVente.venteArticle(unArticle);
+	}
+
+}
