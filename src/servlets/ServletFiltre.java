7540963aa2ed0ab96267f5d2a5bae4b96c505e73
adeloffre <antoine.deloffre2018@campus-eni.fr>
2019-04-30 11:10:09 +0200

    Servlet filtre

---
+++ src/servlets/ServletFiltre.java
@@ -0,0 +1,46 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bo.Enchere;
+import dal.EnchereDAO;
+import dal.UtilisateurDAO;
+
+public class ServletFiltre extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletFiltre() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		EnchereDAO enchereDAO = new EnchereDAO();
+		ArrayList<Enchere> encheres = new ArrayList<>();
+		String categorie = request.getParameter("categorie");
+		String contient = request.getParameter("contient");
+		doGet(request, response);
+	}
+
+}
