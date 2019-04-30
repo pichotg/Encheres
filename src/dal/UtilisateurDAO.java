2d33d91472e5562b1e7874510274b36f0faa8578
Gaspard Pichot <gaspard.pichot2018@campus-eni.fr>
2019-04-30 09:53:51 +0200

    add: UtilisateurDAO => getAllUtilisateur()

--- src/dal/UtilisateurDAO.java
+++ src/dal/UtilisateurDAO.java
@@ -5,15 +5,18 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
 import bo.Utilisateur;
 import jdbc.JDBCTools;
 
 public class UtilisateurDAO {
 	private static final String VERIF_UTILISATEUR = "SELECT * FROM UTILISATEURS where (pseudo = ? OR email = ? ) AND mot_de_passe = ?";
+	private static final String SELECT_ALL_UTILISATEUR = "SELECT * FROM UTILISATEURS";
 	private static final String GET_UTILISATEUR_BY_ID = "SELECT * FROM UTILISATEURS where no_utilisateur = ?";
 	private static final String VERIF_ALREADY_EXIST_UTILISATEUR = "SELECT * FROM UTILISATEURS where pseudo = ? OR email = ?";
 	private static final String INSERT_UTILISATEUR = "INSERT INTO UTILISATEURS (pseudo,nom,prenom,email,telephone,rue,code_postal,ville,mot_de_passe,credit,administrateur) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
-	private static final String MAJ_MDP = "UPDATE UTILISATEURS SET motDePasse = ? WHERE pseudo = ?";
+	private static final String MAJ_ALL = "UPDATE UTILISATEURS SET pseudo = ?,nom = ?,prenom = ?, email = ?, telephone = ?,rue = ?, code_postal = ?, ville = ?, mot_de_passe = ?, credit = ?, administrateur = ? WHERE no_utilisateur = ?";
 
 	public UtilisateurDAO() {
 		super();
@@ -56,16 +59,61 @@ public class UtilisateurDAO {
 
 		return utilisateur;
 	}
+	
+	public ArrayList<Utilisateur> getAllUtilisateur() throws SQLException {
+		ArrayList<Utilisateur> listUser = new ArrayList<Utilisateur>();
+		PreparedStatement preparedStatement = null;
+		Connection conSelect = null;
+		ResultSet rs = null;
 
-	public void updateMDP(String pseudo, String newMDP) throws SQLException {
+		try {
+			conSelect = JDBCTools.getConnection();
+			preparedStatement = conSelect.prepareStatement(SELECT_ALL_UTILISATEUR);
+			rs = preparedStatement.executeQuery();
+
+			if (rs.next()) {
+				listUser.add(new Utilisateur(rs.getInt("no_utilisateur"), rs.getString("pseudo"), rs.getString("nom"),
+						rs.getString("prenom"), rs.getString("email"), rs.getString("telephone"), rs.getString("rue"),
+						rs.getString("code_postal"), rs.getString("ville"), rs.getString("mot_de_passe"),
+						rs.getInt("credit"), rs.getBoolean("administrateur")));
+			}
+
+		} catch (ClassNotFoundException |
+
+				SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null)
+				rs.close();
+			if (preparedStatement != null)
+				preparedStatement.close();
+			if (conSelect != null)
+				conSelect.close();
+		}
+
+		return listUser;
+	}
+
+	public void updateUtilisateur(Utilisateur newutilisateur) throws SQLException {
 		Connection conUpdate = null;
 		PreparedStatement preparedStatement = null;
 		try {
 			conUpdate = JDBCTools.getConnection();
-			preparedStatement = conUpdate.prepareStatement(MAJ_MDP);
-
-			preparedStatement.setString(1, newMDP);
-			preparedStatement.setString(2, pseudo);
+			preparedStatement = conUpdate.prepareStatement(MAJ_ALL);
+			//Set
+			preparedStatement.setString(1, newutilisateur.getPseudo());
+			preparedStatement.setString(2, newutilisateur.getNom());
+			preparedStatement.setString(3, newutilisateur.getPrenom());
+			preparedStatement.setString(4, newutilisateur.getEmail());
+			preparedStatement.setString(5, newutilisateur.getTelephone());
+			preparedStatement.setString(6, newutilisateur.getRue());
+			preparedStatement.setString(7, newutilisateur.getCodePostal());
+			preparedStatement.setString(8, newutilisateur.getVille());
+			preparedStatement.setString(9, newutilisateur.getMotDePasse());
+			preparedStatement.setInt(10, newutilisateur.getCredit());
+			preparedStatement.setBoolean(11, newutilisateur.getAdministrateur());
+			//where
+			preparedStatement.setInt(12, newutilisateur.getNoUtilisateur());
 
 			preparedStatement.executeUpdate();
 
