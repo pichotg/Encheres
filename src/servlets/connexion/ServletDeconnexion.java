68e1670b169cf00b6a2d609ef572f8a18efc4cf1
adeloffre <antoine.deloffre2018@campus-eni.fr>
2019-05-02 12:12:23 +0200

    Correction déconnexion

--- src/servlets/connexion/ServletDeconnexion.java
+++ src/servlets/connexion/ServletDeconnexion.java
@@ -7,6 +7,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 public class ServletDeconnexion extends HttpServlet {
@@ -19,6 +20,8 @@ public class ServletDeconnexion extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Cookie[] cookies;
 		cookies = request.getCookies();
+		HttpSession session = request.getSession();
+		session.setAttribute("utilisateur", null);
 		for (Cookie ck : cookies) {
 			if ("connexion".equals(ck.getName())) {
 				ck.setValue("NULL");
